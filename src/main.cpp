6140202a51fa086ca334a081e26a2eb3d2ff2f99
HAHLIK <saginmatvej836@gmail.com>
2024-07-26 12:04:30 +0700

    OpenGl and GLFW initalization

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,56 @@
 #include <GLAD/glad.h>
 #include <GLFW/glfw3.h>
 
-int main(void)
+
+int g_windowSizeX = 500;
+int g_windowSizeY = 500;
+
+
+void glfwWindowSizeCallBack(GLFWwindow* pWindow, int width, int height)
 {
-    GLFWwindow* window;
+    g_windowSizeX = width;
+    g_windowSizeY = height;
+    glViewport(0, 0, g_windowSizeX, g_windowSizeY);
+}
+
+void glfwKeyCallBack(GLFWwindow* pWindow, int key, int scancode, int action, int mode)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(pWindow, GL_TRUE);
+}
+
+
+int main()
+{
+    GLFWwindow* pWindow;
 
     if (!glfwInit())
         return -1;
 
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    pWindow = glfwCreateWindow(640, 480, "BattleCity", nullptr, nullptr);
+    if (!pWindow)
     {
         glfwTerminate();
         return -1;
     }
+    glfwMakeContextCurrent(pWindow);
 
-    glfwMakeContextCurrent(window);
+    glfwSetWindowSizeCallback(pWindow, glfwWindowSizeCallBack);
+    glfwSetKeyCallback(pWindow, glfwKeyCallBack);
 
-    while (!glfwWindowShouldClose(window))
+    if (!gladLoadGL()) 
+        return -1;
+
+    while (!glfwWindowShouldClose(pWindow))
     {
-        /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+        glfwSwapBuffers(pWindow);
 
-        /* Poll for and process events */
         glfwPollEvents();
     }
 
